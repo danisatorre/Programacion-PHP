cff395eec9bfca74e2d54f6b31371695d38d6ad5
ImDenisc <danisatorrecucart@gmail.com>
2025-01-28 18:46:53 +0100

    IMPOROVEMENT pages.php

--- view/inc/pages.php
+++ view/inc/pages.php
@@ -1,28 +1,33 @@
 <?php
-	switch($_GET['page']){
-		case "homepage";
-			include("module/inicio/view/inicio.php");
-			break;
-		case "controller_course";
-			include("module/course/controller/".$_GET['page'].".php");
-			break;
-		case "services";
-			include("module/services/".$_GET['page'].".php");
-			break;
-		case "aboutus";
-			include("module/aboutus/".$_GET['page'].".php");
-			break;
-		case "contactus";
-			include("module/contact/".$_GET['page'].".php");
-			break;
-		case "404";
-			include("view/inc/error".$_GET['page'].".php");
-			break;
-		case "503";
-			include("view/inc/error".$_GET['page'].".php");
-			break;
-		default;
-			include("module/inicio/view/inicio.php");
-			break;
+	if(isset($_GET['page'])){
+		switch($_GET['page']){
+			case "homepage";
+				include("module/inicio/view/inicio.php");
+				break;
+			case "controller_course";
+				include("module/course/controller/".$_GET['page'].".php");
+				break;
+			case "services";
+				include("module/services/".$_GET['page'].".php");
+				break;
+			case "aboutus";
+				include("module/aboutus/".$_GET['page'].".php");
+				break;
+			case "contactus";
+				include("module/contact/".$_GET['page'].".php");
+				break;
+			case "404";
+				include("view/inc/error".$_GET['page'].".php");
+				break;
+			case "503";
+				include("view/inc/error".$_GET['page'].".php");
+				break;
+			default;
+				include("module/inicio/view/inicio.php");
+				break;
+		}
+	}else{
+		include("module/inicio/view/inicio.php");
 	}
+	
 ?>
