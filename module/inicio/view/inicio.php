476dc69ce5e1a9346ffbf3417a3b7325e6daa330
ImDenisc <danisatorrecucart@gmail.com>
2025-01-31 11:43:19 +0100

    UPDATE homepage

--- module/inicio/view/inicio.php
+++ module/inicio/view/inicio.php
@@ -3,3 +3,44 @@
   <p class="w3-xlarge">Página web para descubrir cursos de programación de manera online y presencial</p>
   <button class="w3-button w3-black w3-padding-large w3-large w3-margin-top"><a href="index.php?page=controller_course&op=create">Crea tu propio curso</a></button>
 </header>
+<div class="w3-row-padding w3-padding-64 w3-container">
+  <div class="w3-content">
+    <div class="w3-twothird">
+      <h1>La página web número 1 en cursos de programación</h1>
+      <h5 class="w3-padding-32">En esta web encontraras desde cursos simples de HTML para gente que este empezando en el mundo de la web, hasta cursos avanzados de múltiples lenguajes de programación como Java, PHP, SQL, ¡y más! para gente más experimentada</h5>
+
+      <p class="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+
+    <div class="w3-third w3-center">
+      <!-- <i class="fa fa-anchor w3-padding-64 w3-text-red"></i> -->
+       <img src="https://cdn-icons-png.flaticon.com/512/10096/10096912.png" style="width: 300px; height: 300px; margin-top: 30%;">
+    </div>
+  </div>
+</div>
+
+<!-- Second Grid -->
+<div class="w3-row-padding w3-light-grey w3-padding-64 w3-container">
+  <div class="w3-content">
+    <div class="w3-third w3-center">
+      <!-- <i class="fa fa-coffee w3-padding-64 w3-text-red w3-margin-right"></i> -->
+       <img src="https://cdn-icons-png.flaticon.com/512/11097/11097313.png" style="width: 300px; height: 300px;">
+    </div>
+
+    <div class="w3-twothird">
+      <h1>¡Retos interactivos!</h1>
+      <h5 class="w3-padding-32">En W3 SCHOOLS disponemos de retos interactivos con los cuales aprender los distintos lenguajes de programación y adentrarte en el mundo de los developers web será más divertido y ameno </h5>
+
+      <p class="w3-text-grey">Entre los nuestros retos interactivos disponemos desde plantillas HTML con CSS y JavaScript para que vayas introduciendote poco a poco en el mundo de la 
+        programación web, hasta retos más avanzados con los cuales podrás demostrar tus habilidades en el mundo de la programación. 
+      Deja de perder el tiempo con tutoriales de internet y aprende a ser developer web con nuestros cursos y retos interactivos</p>
+      </p>
+    </div>
+  </div>
+</div>
+
+<div class="w3-container w3-black w3-center w3-opacity w3-padding-64">
+    <h1 class="w3-margin w3-xlarge">ESCOGE W3 SCHOOLS</h1>
+</div>
